6af17656a551f9c5e86bdeb549b9ed0ebf42e9b5
SanaIshaqat <sanaishaqat@gmail.com>
2021-08-16 21:15:07 +0300

    Css Content page edits

--- js/second.js
+++ js/second.js
@@ -4,10 +4,10 @@ let object_array = JSON.parse(localStorage.data);
 console.log(object_array);
 
 
-for (let i=0;i<object_array.length;i++){
-  let str = '';
-  if (object_array[i].seemore === 1){
-    str += `<div class="sectionTwoDiv">
+for (let i = 0; i < object_array.length; i++) {
+    let str = '';
+    if (object_array[i].seemore === 1) {
+        str += `<div class="sectionTwoDiv">
     <h3>${object_array[i].name}</h3>
   
     <!--image slider start-->
@@ -16,7 +16,7 @@ for (let i=0;i<object_array.length;i++){
             <!--radio buttons start-->
             <input type="radio" name="radio-btn" id="radio11">
             <input type="radio" name="radio-btn" id="radio21">
-            <input type="radio" name="radio-btn" id="radio31">
+            <input type="radio" name="radio-btn" id="radio31}">
             <input type="radio" name="radio-btn" id="radio41">
             <!--radio buttons end-->
             <!--slide images start-->
@@ -44,10 +44,10 @@ for (let i=0;i<object_array.length;i++){
         </div>
         <!--manual navigation start-->
         <div class="navigation-manual">
-        <label for="radio1${i}" class="manual-btn"></label>
-        <label for="radio2${i}" class="manual-btn"></label>
-        <label for="radio3${i}" class="manual-btn"></label>
-        <label for="radio4${i}" class="manual-btn"></label>
+        <label for="radio11" class="manual-btn"></label>
+        <label for="radio21" class="manual-btn"></label>
+        <label for="radio31" class="manual-btn"></label>
+        <label for="radio41" class="manual-btn"></label>
         </div>
         <!--manual navigation end-->
     </div>
@@ -55,22 +55,23 @@ for (let i=0;i<object_array.length;i++){
   
   
     <div class="container">
-  
-        <h4>Main Resturants</h4>
+       <div class="rest">
+            <h4>Top Resturant Picks</h4>
         <ul>
-            <li>Resturant#1: ${object_array[i].resturants[0]}</li>
-            <li>Resturant#2: ${object_array[i].resturants[1]}</li>
-            <li>Resturant#3: ${object_array[i].resturants[2]}</li>
-            <li>Resturant#4: ${object_array[i].resturants[3]}</li>
+            <li><h5>Resturant Option 1:</h5> ${object_array[i].resturants[0]}</li>
+            <li><h5>Resturant Option 2:</h5> ${object_array[i].resturants[1]}</li>
+            <li><h5>Resturant Option 3:</h5> ${object_array[i].resturants[2]}</li>
+            <li><h5>Resturant Option 4:</h5> ${object_array[i].resturants[3]}</li>
         </ul>
+       </div> 
         <!--image slider start-->
     <div class="slider">
         <div class="slides">
             <!--radio buttons start-->
-            <input type="radio" name="radio-btn" id="radio11">
-            <input type="radio" name="radio-btn" id="radio21">
-            <input type="radio" name="radio-btn" id="radio31">
-            <input type="radio" name="radio-btn" id="radio41">
+            <input type="radio" name="radio-btn" id="radio12">
+            <input type="radio" name="radio-btn" id="radio22">
+            <input type="radio" name="radio-btn" id="radio32">
+            <input type="radio" name="radio-btn" id="radio42">
             <!--radio buttons end-->
             <!--slide images start-->
             <div class="slide first">
@@ -97,30 +98,31 @@ for (let i=0;i<object_array.length;i++){
         </div>
         <!--manual navigation start-->
         <div class="navigation-manual">
-        <label for="radio1${i}" class="manual-btn"></label>
-        <label for="radio2${i}" class="manual-btn"></label>
-        <label for="radio3${i}" class="manual-btn"></label>
-        <label for="radio4${i}" class="manual-btn"></label>
+        <label for="radio12" class="manual-btn"></label>
+        <label for="radio22" class="manual-btn"></label>
+        <label for="radio32" class="manual-btn"></label>
+        <label for="radio42" class="manual-btn"></label>
         </div>
         <!--manual navigation end-->
     </div>
     <!--image slider end-->
-        <h4>Best Hotels</h4>
+    <div class="hotel">
+        <h4>Best Rated Hotels</h4>
         <ul>
-            <li>Hotel#1: ${object_array[i].hotels[0]}</li>
-            <li>Hotel#2: ${object_array[i].hotels[1]}</li>
-            <li>Hotel#3: ${object_array[i].hotels[2]}</li>
-            <li>Hotel#4: ${object_array[i].hotels[3]}</li>
+            <li><h5>Hotel Option 1:</h5> ${object_array[i].hotels[0]}</li>
+            <li><h5>Hotel Option 2:</h5> ${object_array[i].hotels[1]}</li>
+            <li><h5>Hotel Option 3:</h5> ${object_array[i].hotels[2]}</li>
+            <li><h5>Hotel Option 4:</h5> ${object_array[i].hotels[3]}</li>
         </ul>
-
+</div>
         <!--image slider start-->
         <div class="slider">
             <div class="slides">
                 <!--radio buttons start-->
-                <input type="radio" name="radio-btn" id="radio11">
-                <input type="radio" name="radio-btn" id="radio21">
-                <input type="radio" name="radio-btn" id="radio31">
-                <input type="radio" name="radio-btn" id="radio41">
+                <input type="radio" name="radio-btn" id="radio13">
+            <input type="radio" name="radio-btn" id="radio23">
+            <input type="radio" name="radio-btn" id="radio33">
+            <input type="radio" name="radio-btn" id="radio43">
                 <!--radio buttons end-->
                 <!--slide images start-->
                 <div class="slide first">
@@ -147,21 +149,23 @@ for (let i=0;i<object_array.length;i++){
             </div>
             <!--manual navigation start-->
             <div class="navigation-manual">
-            <label for="radio1${i}" class="manual-btn"></label>
-            <label for="radio2${i}" class="manual-btn"></label>
-            <label for="radio3${i}" class="manual-btn"></label>
-            <label for="radio4${i}" class="manual-btn"></label>
+            <label for="radio13" class="manual-btn"></label>
+            <label for="radio23" class="manual-btn"></label>
+            <label for="radio33" class="manual-btn"></label>
+            <label for="radio43" class="manual-btn"></label>
             </div>
             <!--manual navigation end-->
         </div>
         <!--image slider end-->
-        <h4>Landmarks</h4>
+        <div class="land">
+        <h4>Must Go To Landmarks</h4>
         <ul>
-            <li>landmark#1: ${object_array[i].landmarks[0]}</li>
-            <li>landmark#2: ${object_array[i].landmarks[1]}</li>
-            <li>landmark#3: ${object_array[i].landmarks[2]}</li>
-            <li>landmark#4: ${object_array[i].landmarks[3]}</li>
-        </ul>;`;
-    document.getElementById('second').innerHTML = str;
-  }
+            <li><h5>landmark Option 1:</h5> ${object_array[i].landmarks[0]}</li>
+            <li><h5>landmark Option 2:</h5> ${object_array[i].landmarks[1]}</li>
+            <li><h5>landmark Option 3:</h5> ${object_array[i].landmarks[2]}</li>
+            <li><h5>landmark Option 4:</h5> ${object_array[i].landmarks[3]}</li>
+        </ul>
+        </div>;`;
+        document.getElementById('second').innerHTML = str;
+    }
 }
